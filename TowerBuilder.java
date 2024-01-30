914771087725660aca20c4701b463fe930b9c2b4
AyubAbdGani <ayub.abdgani@gmail.com>
2024-01-30 16:55:11 +0800

    Menambahkan TowerBuilder dengan bahasa Java

---
+++ TowerBuilder.java
@@ -0,0 +1,23 @@
+import java.math.*;
+import java.util.*;
+public class TowerBuilder {
+    public static void main(String... args) {
+        System.out.println(Arrays.toString(towerBuilder(6)));
+    }
+    public static String[] towerBuilder(int nFloors){
+        String[] result = new String[nFloors];
+        for (int i = 0; i < nFloors; i++){
+            StringBuilder build = new StringBuilder();
+            int totalKolom = nFloors*2 -1;
+            for (int j = 0 ; j < totalKolom; j++){
+                if (j >= nFloors -1 - i && j < nFloors  + i ){
+                    build.append('*');
+                }else{
+                    build.append(' ');
+                }
+            }
+            result[i] = build.toString();
+        }
+        return result;      
+    }
+}
