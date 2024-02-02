bf9500baa73a2dc86798238c5707937ec788c33b
AyubAbdGani <ayub.abdgani@gmail.com>
2024-02-02 09:46:24 +0800

    Menambahkan NumberOfTrailingZerosOfN dan MaximumSubarraySum dalam bahasa java

---
+++ MaximumSubarraySum.java
@@ -0,0 +1,54 @@
+import java.util.Arrays;
+
+public class MaximumSubarraySum {
+    public static void main(String[] args) {
+
+        System.out.println(sequence(new int[]{-2, 1, -3, 4, -1, 2, 1, -5, 4}));
+        
+    }
+
+    public static int sequence(int[] arr) {
+        if (arr == null || arr.length == 0){
+            return 0;
+        }
+
+        int result = Integer.MIN_VALUE;
+        int startPoint = 0;
+        int range = 2;
+        boolean right = true;
+
+        for(;;){
+            int temp = 0;
+
+            for (int i = startPoint; i < range + startPoint; i++){
+                temp += arr[i];
+            }
+            
+            if (temp > result){
+                result = temp;
+            }
+
+            if (right){
+                startPoint++;
+                if (startPoint + range >= arr.length){
+                    right = false;
+                    startPoint = arr.length - range;
+                }
+            }else{
+                startPoint--;
+                if (startPoint - range < 0){
+                    right = true;
+                    startPoint = 0;
+                    range++;
+                    if (range > arr.length){
+                        break;
+                    }
+                }
+            }
+            
+
+        }
+
+        return result < 0 ? 0 : result;
+    }
+}
---
+++ NumberOfTrailingZerosOfN.java
@@ -0,0 +1,15 @@
+public class NumberOfTrailingZerosOfN {
+    public static void main(String[] args) {
+        System.out.println(zeros(100));
+    }
+
+    public static int zeros(int n) {
+        int rz = 0;
+
+        for (int i = 5; i <= n; i *= 5){
+            rz += n/i;
+        }
+
+        return rz;
+    }
+}
